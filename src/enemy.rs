8308862bf1862267721386cf6789a75ffd829c15
arlohb <arlo@urwick.co.uk>
2024-08-17 14:28:12 +0100

    Added very basic enemy movement

--- src/enemy.rs
+++ src/enemy.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use crate::{
     elements::ElementInfo,
     molecule::{BuildMolecule, Molecule},
-    Velocity,
+    Player, Velocity,
 };
 
 #[derive(Component)]
@@ -16,7 +16,7 @@ pub fn create_enemy(mut cmds: Commands, mut build_molecule_event: EventWriter<Bu
             Velocity {
                 velocity: Vec3::ZERO,
                 drag: 0.04,
-                max_speed: None,
+                max_speed: Some(50.),
             },
             Molecule {
                 elements: vec![ElementInfo::Uranium],
@@ -28,6 +28,29 @@ pub fn create_enemy(mut cmds: Commands, mut build_molecule_event: EventWriter<Bu
     build_molecule_event.send(BuildMolecule(id));
 }
 
+pub fn enemy_movement_system(
+    mut enemies: Query<(&mut Velocity, &Transform), With<Enemy>>,
+    players: Query<&Transform, With<Player>>,
+) {
+    let speed = 0.4;
+
+    let player = players.single().translation.xy();
+
+    for (mut velocity, origin) in &mut enemies {
+        let offset = player - origin.translation.xy();
+
+        if offset.length() <= 0.0001 {
+            continue;
+        }
+
+        let dir = offset.normalize();
+        let accel = speed * dir;
+
+        velocity.velocity += Vec3::new(accel.x, accel.y, 0.);
+    }
+}
+
 pub fn plugin(app: &mut App) {
-    app.add_systems(Startup, create_enemy);
+    app.add_systems(Startup, create_enemy)
+        .add_systems(FixedUpdate, enemy_movement_system);
 }

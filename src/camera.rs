90f00095d74debb6dcf8712ab3929c99d8f01c12
arlohb <arlo@urwick.co.uk>
2024-08-19 22:06:16 +0100

    Added custom cursor

--- src/camera.rs
+++ src/camera.rs
@@ -42,11 +42,58 @@ pub fn zoom_camera(
     camera.scale = camera.scale.lerp(target, 0.1);
 }
 
+#[derive(Component)]
+pub struct Cursor;
+
+const CURSOR_SIZE: f32 = 186.;
+
+pub fn setup_cursor(mut cmds: Commands, assets: Res<AssetServer>, mut windows: Query<&mut Window>) {
+    let Ok(mut window) = windows.get_single_mut() else {
+        return;
+    };
+
+    window.cursor.visible = false;
+
+    cmds.spawn((
+        ImageBundle {
+            style: Style {
+                width: Val::Px(CURSOR_SIZE),
+                height: Val::Px(CURSOR_SIZE),
+                ..Default::default()
+            },
+            image: UiImage {
+                texture: assets.load("Cursor.png"),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Cursor,
+    ));
+}
+
+pub fn update_cursor(mut cursors: Query<&mut Style, With<Cursor>>, windows: Query<&Window>) {
+    let Ok(mut cursor) = cursors.get_single_mut() else {
+        return;
+    };
+
+    let Ok(window) = windows.get_single() else {
+        return;
+    };
+
+    let Some(pos) = window.cursor_position() else {
+        return;
+    };
+
+    cursor.left = Val::Px(pos.x - CURSOR_SIZE / 2.);
+    cursor.top = Val::Px(pos.y - CURSOR_SIZE / 2.);
+}
+
 pub fn plugin(app: &mut App) {
-    app.add_systems(Startup, setup_camera)
+    app.add_systems(Startup, (setup_camera, setup_cursor))
         .add_systems(
             FixedPostUpdate,
             follow_system::<MainCamera, Player, 10>.after(velocity::apply_velocity),
         )
-        .add_systems(Update, zoom_camera);
+        .add_systems(Update, zoom_camera)
+        .add_systems(Update, update_cursor);
 }

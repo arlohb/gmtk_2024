e4a42811950ea79e490e58219af83c592edb5ec3
arlohb <arlo@urwick.co.uk>
2024-08-20 17:00:06 +0100

    Fixed web build

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,7 @@ fn setup_music(mut cmds: Commands, assets: Res<AssetServer>) {
 }
 
 fn main() {
-    std::env::set_var("RUST_BACKTRACE", "1");
+    // std::env::set_var("RUST_BACKTRACE", "1");
 
     App::new()
         // Enable ambiguity detection
